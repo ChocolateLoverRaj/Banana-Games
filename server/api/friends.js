d20fa01bdd96bdbbd87d372281463aa9a1e49a87
Rajas Paranjpe <paranjperajas@gmail.com>
2019-12-12 17:23:29 -0600

    Done with friends API

--- server/api/friends.js
+++ server/api/friends.js
@@ -19,9 +19,6 @@ friends.process = (data, callback) => {
     }
 };
 
-//List
-friends.list = {};
-
 //id to user
 friends.idToUser = (ids, callback) => {
     //Check if ids has a length of 0
@@ -59,6 +56,32 @@ friends.idToUser = (ids, callback) => {
         callback(false, []);
     }
 };
+
+//Make two people each others' friends
+friends.friendship = (id1, id2, callback) => {
+    //Operations on users collection
+    const usersCollection = mongodb.collection("BananaGames", "Users");
+    //Make id2 id1's friend
+    usersCollection.findOneAndUpdate({ id: id1 }, { $push: { friends: id2 } }, (err, res) => {
+        if (!err && res.value) {
+            //Make id1 id2's friend
+            usersCollection.findOneAndUpdate({ id: id2 }, { $push: { friends: id1 } }, (err, res) => {
+                if (!err && res.value) {
+                    callback(false);
+                }
+                else {
+                    callback("Could not add you to your friend\'s friends list");
+                }
+            });
+        }
+        else {
+            callback("Could not add your friend to your friends list");
+        }
+    });
+};
+
+//List
+friends.list = {};
 
 //List process
 friends.list.process = (data, callback) => {
@@ -228,17 +251,51 @@ friends.invites.post = (data, callback) => {
                                                 if (!res) {
                                                     //Database time
                                                     const friendsCollection = mongodb.collection("BananaGames", "FriendRequests");
-                                                    //Request
-                                                    let invite = {
-                                                        from: token.userId,
-                                                        to: id
-                                                    };
-                                                    friendsCollection.insertOne(invite, (err, res) => {
-                                                        if (!err && res && res.result) {
-                                                            callback(201);
+                                                    //Make sure that there isn't already a request for that same friend
+                                                    friendsCollection.findOne({ from: token.userId, to: id }, (err, res) => {
+                                                        if (!err) {
+                                                            if (!res) {
+                                                                //Make sure that the friend that the user is trying to friend hasn't already friend requested them. If they have, then automatically friend the two of them and delelete the friend's request.
+                                                                friendsCollection.findOneAndDelete({ from: id, to: token.userId }, (err, res) => {
+                                                                    if (!err) {
+                                                                        if (res && res.result) {
+                                                                            //Friendship
+                                                                            friends.friendship(id, token.userId, err => {
+                                                                                if (!err) {
+                                                                                    callback(200);
+                                                                                }
+                                                                                else {
+                                                                                    callback(500, { "Error": "Could not friend that person" });
+                                                                                }
+                                                                            });
+                                                                        }
+                                                                        else {
+                                                                            //Invite
+                                                                            let invite = {
+                                                                                from: token.userId,
+                                                                                to: id
+                                                                            };
+                                                                            friendsCollection.insertOne(invite, (err, res) => {
+                                                                                if (!err && res && res.result) {
+                                                                                    callback(201);
+                                                                                }
+                                                                                else {
+                                                                                    callback(500, { "Error": "Could not invite friend" });
+                                                                                }
+                                                                            });
+                                                                        }
+                                                                    }
+                                                                    else {
+                                                                        callback(500, { "Error": "Could not check if person has requested you" });
+                                                                    }
+                                                                });
+                                                            }
+                                                            else {
+                                                                callback(409, { "Error": "You already sent a request to that person" });
+                                                            }
                                                         }
                                                         else {
-                                                            callback(500, { "Error": "Could not invite friend" });
+                                                            callback(500, { "Error": "Could not check friend requests" });
                                                         }
                                                     });
                                                 }
@@ -263,6 +320,268 @@ friends.invites.post = (data, callback) => {
                         else {
                             callback(400, { "Error": "Sorry, you can\'t friend yourself!" });
                         }
+                    }
+                    else {
+                        callback(401);
+                    }
+                }
+                else {
+                    callback(500, { "Error": "Could not check token" });
+                }
+            });
+        }
+        else {
+            callback(403);
+        }
+    }
+    else {
+        callback(400, { "Error": "Missing id in query" });
+    }
+};
+
+//Invites get
+friends.invites.get = (data, callback) => {
+    //Validate
+    let token = typeof (data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
+
+    //Check for token
+    if (token) {
+        //Make sure token is good
+        tokens.check(token, (err, token) => {
+            if (!err) {
+                if (token) {
+                    //Database time
+                    const friendsCollection = mongodb.collection("BananaGames", "FriendRequests");
+                    friendsCollection.find({ from: token.userId }).toArray((err, invites) => {
+                        if (!err && invites) {
+                            if (invites) {
+                                var idList = [];
+                                invites.forEach(invite => {
+                                    idList.push(invite.to);
+                                });
+                                friends.idToUser(idList, (err, list) => {
+                                    if (!err && list) {
+                                        callback(200, list);
+                                    }
+                                    else {
+                                        callback(500, { "Error": "Couldn't get friends\' data" });
+                                    }
+                                });
+                            }
+                        }
+                        else {
+                            callback(500, { "Error": "Could not find friends" });
+                        }
+                    });
+                }
+                else {
+                    callback(401);
+                }
+            }
+            else {
+                callback(500, { "Error": "Could not check token" });
+            }
+        });
+    }
+    else {
+        callback(403);
+    }
+};
+
+//Invites delete
+friends.invites.delete = (data, callback) => {
+    //Validate
+    let token = typeof (data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
+    let id = typeof (data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length > 0 ? data.queryStringObject.id.trim() : false;
+
+    if (id) {
+        //Check for token
+        if (token) {
+            //Make sure token is good
+            tokens.check(token, (err, token) => {
+                if (!err) {
+                    if (token) {
+                        //Make sure request to that id exists
+                        const friendsCollection = mongodb.collection("BananaGames", "FriendRequests");
+                        friendsCollection.findOneAndDelete({ from: token.userId, to: id }, (err, res) => {
+                            if (!err) {
+                                if (res && res.value) {
+                                    callback(200);
+                                }
+                                else {
+                                    callback(404, { "Error": "Invite with that id does not exists" });
+                                }
+                            }
+                            else {
+                                callback(500, { "Error": "Couldn\'t find friend request" });
+                            }
+                        });
+                    }
+                    else {
+                        callback(401);
+                    }
+                }
+                else {
+                    callback(500, { "Error": "Could not check token" });
+                }
+            });
+        }
+        else {
+            callback(403);
+        }
+    }
+    else {
+        callback(400, { "Error": "Missing id in query" });
+    }
+};
+
+//Requests
+friends.requests = {};
+
+//Requests process
+friends.requests.process = (data, callback) => {
+    //Acceptable methods
+    const acceptableMethods = ["get", "put", "delete"];
+    if (acceptableMethods.indexOf(data.method) > -1) {
+        friends.requests[data.method](data, callback);
+    }
+    else {
+        callback(405);
+    }
+};
+
+//Requests get
+friends.requests.get = (data, callback) => {
+    //Validate
+    let token = typeof (data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
+
+    //Check for token
+    if (token) {
+        //Make sure token is good
+        tokens.check(token, (err, token) => {
+            if (!err) {
+                if (token) {
+                    //Database time
+                    const friendsCollection = mongodb.collection("BananaGames", "FriendRequests");
+                    friendsCollection.find({ to: token.userId }).toArray((err, invites) => {
+                        if (!err && invites) {
+                            if (invites) {
+                                var idList = [];
+                                invites.forEach(invite => {
+                                    idList.push(invite.from);
+                                });
+                                friends.idToUser(idList, (err, list) => {
+                                    if (!err && list) {
+                                        callback(200, list);
+                                    }
+                                    else {
+                                        callback(500, { "Error": "Couldn't get friends\' data" });
+                                    }
+                                });
+                            }
+                        }
+                        else {
+                            callback(500, { "Error": "Could not find friends" });
+                        }
+                    });
+                }
+                else {
+                    callback(401);
+                }
+            }
+            else {
+                callback(500, { "Error": "Could not check token" });
+            }
+        });
+    }
+    else {
+        callback(403);
+    }
+};
+
+//Requests put
+friends.requests.put = (data, callback) => {
+    //Validate
+    let token = typeof (data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
+    let id = typeof (data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length > 0 ? data.queryStringObject.id.trim() : false;
+
+    if (id) {
+        //Check for token
+        if (token) {
+            //Make sure token is good
+            tokens.check(token, (err, token) => {
+                if (!err) {
+                    if (token) {
+                        //Try to find the request, and then delete it because the request won't be needed anymore
+                        const friendsCollection = mongodb.collection("BananaGames", "FriendRequests");
+                        friendsCollection.findOneAndDelete({ from: id, to: token.userId }, (err, res) => {
+                            if (!err) {
+                                if (res && res.value) {
+                                    //Update both the users
+                                    friends.friendship(id, token.userId, err => {
+                                        if (!err) {
+                                            callback(200);
+                                        }
+                                        else {
+                                            callback(500, { "Error": "Could not accept friend request"});
+                                        }
+                                    });
+                                }
+                                else {
+                                    callback(404, { "Error": "That person did not invite you" });
+                                }
+                            }
+                            else {
+                                callback(500, { "Error": "Could not find the request" });
+                            }
+                        });
+                    }
+                    else {
+                        callback(401);
+                    }
+                }
+                else {
+                    callback(500, { "Error": "Could not check token" });
+                }
+            });
+        }
+        else {
+            callback(403);
+        }
+    }
+    else {
+        callback(400, { "Error": "Missing id in query" });
+    }
+};
+
+//Requests delete
+friends.requests.delete = (data, callback) => {
+    //Validate
+    let token = typeof (data.headers.token) == 'string' && data.headers.token.trim().length > 0 ? data.headers.token.trim() : false;
+    let id = typeof (data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length > 0 ? data.queryStringObject.id.trim() : false;
+
+    if (id) {
+        //Check for token
+        if (token) {
+            //Make sure token is good
+            tokens.check(token, (err, token) => {
+                if (!err) {
+                    if (token) {
+                        //Make sure request to that id exists
+                        const friendsCollection = mongodb.collection("BananaGames", "FriendRequests");
+                        friendsCollection.findOneAndDelete({ to: token.userId, from: id }, (err, res) => {
+                            if (!err) {
+                                if (res && res.value) {
+                                    callback(200);
+                                }
+                                else {
+                                    callback(404, { "Error": "Invite with that id does not exists" });
+                                }
+                            }
+                            else {
+                                callback(500, { "Error": "Couldn\'t find friend request" });
+                            }
+                        });
                     }
                     else {
                         callback(401);
