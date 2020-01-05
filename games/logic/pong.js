79ae4c8f39dab3a39081b65ed9b0387dddb9782f
Rajas Paranjpe <paranjperajas@gmail.com>
2020-01-05 12:42:16 -0600

    Started Making Classic Map

--- games/logic/pong.js
+++ games/logic/pong.js
@@ -1,6 +1,32 @@
 //Logic for the game pong
 
 //Logic Module
-const logic = {
-    
-};
+const logic = {};
+
+//Maps
+logic.maps = [
+    {
+        name: "Classic",
+        players: 2,
+        entities: {
+            walls: [
+                {
+                    position: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 600,
+                        y2: 0,
+                    }
+                },
+                {
+                    position: {
+                        x1: 0,
+                        y1: 400,
+                        x2: 600,
+                        y2: 400,
+                    }
+                }
+            ]
+        }
+    }
+];
