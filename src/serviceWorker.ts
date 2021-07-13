00d85b7b38adfcc4a4cede40a5f8643d1eefb977
Rajas Paranjpe <paranjperajas@gmail.com>
2021-07-13 15:58:13 -0500

    Better logic for finding necessary chunks

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -4,9 +4,13 @@ self.addEventListener('install', (e: any) => {
     .then(async res => await res.json())
     .then(async stats => {
       console.log(stats)
+      const appChunk = stats.chunks.find(({ names }) => names.includes('app'))
+      console.log('app chunk', appChunk)
+      const mainChunks = stats.chunks.filter(({ id }) => appChunk.siblings.includes(id))
+      console.log('main chunks', mainChunks)
       const filesToCache = [
         './',
-        ...stats.namedChunkGroups.app.assets.map(({ name }) => name)
+        ...[appChunk, ...mainChunks].flatMap(({ files }) => files)
       ]
       // Delete old caches
       await Promise.all((await caches.keys()).map(async key => await caches.delete(key)))
