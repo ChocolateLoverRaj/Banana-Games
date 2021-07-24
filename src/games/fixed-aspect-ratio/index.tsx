77fa217b4139d2c0a6d8018b0f39eb2448d853e1
Rajas Paranjpe <paranjperajas@gmail.com>
2021-07-24 07:56:56 -0500

    `fixed-aspect-ratio` description

--- src/games/fixed-aspect-ratio/index.tsx
+++ src/games/fixed-aspect-ratio/index.tsx
@@ -6,6 +6,7 @@ import GameComponent from '../../types/GameComponent'
 import getScaledSize from '../../util/getScaledSize'
 import Size from '../../util/types/Size'
 import sizeToString from '../../util/sizeToString'
+import { Typography } from 'antd'
 
 const ratio: Size = {
   width: 16,
@@ -32,5 +33,24 @@ const FixedAspectRatio: GameComponent = forwardRef((_props, ref) => {
     </div>
   )
 })
+
+FixedAspectRatio.description = (
+  <>
+    Many games require a specific aspect ratio to play. In this website, games which use fixed
+    aspect ratios will grow as big as possible while maintaining the same aspect ratio.
+    <br /><br />
+    The aspect ratio <a href='https://en.wikipedia.org/wiki/16:9_aspect_ratio'>16:9</a> is used in
+    this game because it is commonly used.
+    <Typography.Paragraph>
+      <blockquote>
+        Once seen as exotic, since 2009, it has become the most common aspect ratio for televisions
+        and computer monitors and is also the international standard format of digital television
+        HDTV Full HD and SD TV. It has replaced the fullscreen 4:3 aspect ratio.
+      </blockquote>
+    </Typography.Paragraph>
+    On many screens this game will take up the entire
+    screen in full screen mode.
+  </>
+)
 
 export default FixedAspectRatio
