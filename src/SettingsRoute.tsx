5bfda320ec7ef15bf69285fb920496415aec9653
Rajas Paranjpe <paranjperajas@gmail.com>
2021-08-06 13:09:59 -0700

    Improved settings changing

--- src/SettingsRoute.tsx
+++ src/SettingsRoute.tsx
@@ -1,5 +1,5 @@
-import { FC, useState } from 'react'
-import { Button, Form, Switch, Typography, Spin, FormProps } from 'antd'
+import { FC, useEffect } from 'react'
+import { Form, Switch, Typography, Spin, FormProps } from 'antd'
 import { useTransaction } from './util/use-indexed-db'
 import settingsDb from './settingsDb'
 import usePromise from 'react-use-promise'
@@ -7,20 +7,27 @@ import ErrorResult from './ErrorResult'
 import Helmet from 'react-helmet'
 import config from './config.json'
 import storeToObject from './util/storeToObject'
+import { useMapState } from 'rooks'
+import { NamePath } from 'rc-field-form/lib/interface'
 
 const SettingsRoute: FC = () => {
   const createTransaction = useTransaction(settingsDb)
   const [settings, error] = usePromise<object>(async () => await storeToObject(
     (await createTransaction(['settings'], 'readonly')).objectStore('settings')), [])
-  const [savePromise, setSavePromise] = useState<Promise<void>>(Promise.resolve())
-  const [, saveError, saveState] = usePromise(savePromise, [savePromise])
-
-  const handleFinish: FormProps['onFinish'] = settings =>
-    setSavePromise(createTransaction(['settings'], 'readwrite').then(async transaction => {
-      const store = transaction.objectStore('settings')
-      // TODO: Only modify changed fields
-      await Promise.all(Object.entries(settings).map(async ([k, v]) => await store.put?.(v, k)))
-    }))
+  const savePromises = useMapState(new Map<string, Promise<void>>())
+  const saveErrors = useMapState(new Map<string, Error>())
+  const handleChange: FormProps['onFieldsChange'] = ([{ name, value }]) => {
+    const key = (name as NamePath)[0]
+    savePromises.set(key, (async () => {
+      const transaction = await createTransaction(['settings'], 'readwrite')
+      await transaction.objectStore('settings').put?.(value, key)
+    })())
+  }
+  useEffect(() => {
+    [...savePromises].forEach(([key, savePromise]) => {
+      savePromise.then(() => savePromises.delete(key), e => saveErrors.set(key, e))
+    })
+  }, [savePromises])
 
   return (
     <>
@@ -30,16 +37,19 @@ const SettingsRoute: FC = () => {
       <div>
         <Typography.Title>Settings</Typography.Title>
         {settings !== undefined
-          ? saveError === undefined
+          ? saveErrors.size === 0
             ? (
               // TODO: Show unsaved / unchanged fields
-              <Form initialValues={settings} onFinish={handleFinish}>
+              <Form
+                initialValues={settings}
+                onFieldsChange={handleChange}
+              >
                 <Form.Item
                   name='pausedWhenNotVisible'
                   label='Paused When Not Visible'
                   valuePropName='checked'
                 >
-                  <Switch />
+                  <Switch loading={savePromises.has('pausedWhenNotVisible')} />
                 </Form.Item>
                 <section id='warnBeforeLeavingGame' />
                 <Form.Item
@@ -47,20 +57,11 @@ const SettingsRoute: FC = () => {
                   label='Warn Before Leaving Game'
                   valuePropName='checked'
                 >
-                  <Switch />
-                </Form.Item>
-                <Form.Item>
-                  <Button
-                    htmlType='submit'
-                    type='primary'
-                    loading={saveState === 'pending'}
-                  >
-                    Save Changes
-                  </Button>
+                  <Switch loading={savePromises.has('warnBeforeLeavingGame')} />
                 </Form.Item>
               </Form>
               )
-            : <ErrorResult title='Error saving settings' error={saveError} />
+            : <ErrorResult title='Error saving settings' error={new Error('not implemented')} />
           : error === undefined
             ? <Spin tip='Loading Settings' />
             : <ErrorResult title='Error loading settings' error={error} />}
